f73b2bb2124e37b20fe3727277b0a18b30e18038
Scipo <ivots@mail.bg>
2016-07-29 17:06:08 +0300

    New classes LocationAndCoordinates and WeatherDataCollectonAbstract

---
+++ app/src/main/java/com/androidweather/newappweather/LocationAndCoordinates.java
@@ -0,0 +1,79 @@
+package com.androidweather.newappweather;
+
+/**
+ * Created by Home on 29.7.2016 г..
+ */
+public class LocationAndCoordinates {
+    private float longitude;
+    private float latitude;
+    private String country;
+    private String city;
+    private long sunset;
+    private long sunrise;
+
+    public LocationAndCoordinates(){
+        this.longitude = 0;
+        this.latitude = 0;
+        this.country = "NoCountry";
+        this.city = "NoCity";
+        this.sunset = 0;
+        this.sunrise = 0;
+     }
+
+    public LocationAndCoordinates(float longitude, float latitude, String country, String city, long sunset, long sunrise) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.country = country;
+        this.city = city;
+        this.sunset = sunset;
+        this.sunrise = sunrise;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setSunset(long sunset) {
+        this.sunset = sunset;
+    }
+
+    public void setSunrise(long sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public long getSunset() {
+        return sunset;
+    }
+
+    public long getSunrise() {
+        return sunrise;
+    }
+}
---
+++ app/src/main/java/com/androidweather/newappweather/WeatherDataCollectionAbstract.java
@@ -0,0 +1,61 @@
+package com.androidweather.newappweather;
+
+import org.json.JSONObject;
+/**
+ * Created by Home on 29.7.2016 г..
+ */
+abstract public class WeatherDataCollectionAbstract {
+    protected static final String JDateTime = "JDT";
+    protected  static final String JWind= "JWind";
+    protected  static final String JRain="JRain";
+    protected static  final String JSnow="JSnow";
+
+    static abstract public class Temperature{
+
+        protected static final String JTemperature="tmp";
+        protected static final String JTemperatureMin="tmp_min";
+        protected static final String JTemperatureMAX="tmp_max";
+
+        abstract public float getTemp ();
+        abstract public float getTempMin ();
+        abstract public float getTempMax ();
+    }
+
+    static abstract public class Humidity{
+        protected static final String JHumidity="Humidity";
+        protected static final String JPressure="Pressure";
+
+        abstract public float getPressure ();
+        abstract public float getHumidity ();
+    }
+    static abstract public class Wind{
+
+        protected static final String JSpeed= "speed";
+        protected static final String JDeg = "deg";
+        protected static final String JGust= "gust";
+        protected static final String JBeg = "beg";
+        protected static final String JEnd = "end";
+
+        abstract public float getSpeed ();
+        abstract public int getDeg ();
+        abstract public float getGust ();
+        abstract public int getBeg ();
+        abstract public int getEnd ();
+    }
+
+    abstract public float getTemp ();
+    abstract public float getHumidity ();
+    abstract public float getPressure ();
+    abstract public float getWindSpeed ();
+    abstract public float getWindGust ();
+    abstract public int getWindDeg ();
+    abstract public int getRain ();
+    abstract public int getSnow ();
+    abstract public int getPrecipitation ();
+
+}
+
+
+
+
+
